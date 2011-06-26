3283467a74e514c3455c248e7218e717ab5c1adc
Fábio Miranda Costa <fabiomcosta@gmail.com>
2011-06-26 00:09:59 -0300

    just changing from tabs to spaces

--- src/uSelector.js
+++ src/uSelector.js
@@ -7,144 +7,144 @@
 
 (function(global, document){
 
-	var elements,
-		parsed,
-		parsedClasses,
-		parsedPseudos,
-		pseudos = {},
-		context,
-		currentDocument,
-		reTrim = /^\s+|\s+$/g;
+    var elements,
+        parsed,
+        parsedClasses,
+        parsedPseudos,
+        pseudos = {},
+        context,
+        currentDocument,
+        reTrim = /^\s+|\s+$/g;
 
-	var supports_querySelectorAll = !!document.querySelectorAll;
+    var supports_querySelectorAll = !!document.querySelectorAll;
 
-	var $u = function(selector, _context, append){
-		elements = append || [];
-		context = _context || $u.context;
-		if (supports_querySelectorAll){
-			try{
-				arrayFrom(context.querySelectorAll(selector));
-				return elements;
-			} catch (e){}
-		}
+    var $u = function(selector, _context, append){
+        elements = append || [];
+        context = _context || $u.context;
+        if (supports_querySelectorAll){
+            try{
+                arrayFrom(context.querySelectorAll(selector));
+                return elements;
+            } catch (e){}
+        }
 
-		currentDocument = context.ownerDocument || context;
-		parse(selector.replace(reTrim, ''));
-		find();
+        currentDocument = context.ownerDocument || context;
+        parse(selector.replace(reTrim, ''));
+        find();
 
-		return elements;
-	};
+        return elements;
+    };
 
-	var matchSelector = function(node){
-		if (parsed.tag){
-			var nodeName = node.nodeName.toUpperCase();
-			if (parsed.tag == '*'){
-				if (nodeName < '@') return false; // Fix for comment nodes and closed nodes
-			} else {
-				if (nodeName != parsed.tag) return false;
-			}
-		}
+    var matchSelector = function(node){
+        if (parsed.tag){
+            var nodeName = node.nodeName.toUpperCase();
+            if (parsed.tag == '*'){
+                if (nodeName < '@') return false; // Fix for comment nodes and closed nodes
+            } else {
+                if (nodeName != parsed.tag) return false;
+            }
+        }
 
-		if (parsed.id && node.getAttribute('id') != parsed.id){
-			return false;
-		}
+        if (parsed.id && node.getAttribute('id') != parsed.id){
+            return false;
+        }
 
-		if ((parsedClasses = parsed.classes)){
-			var className = (' ' + node.className + ' ');
-			for (var i = parsedClasses.length; i--;){
-				if (className.indexOf(' ' + parsedClasses[i] + ' ') < 0) return false;
-			}
-		}
+        if ((parsedClasses = parsed.classes)){
+            var className = (' ' + node.className + ' ');
+            for (var i = parsedClasses.length; i--;){
+                if (className.indexOf(' ' + parsedClasses[i] + ' ') < 0) return false;
+            }
+        }
 
-		if ((parsedPseudos = parsed.pseudos)){
-			for (var i = parsedPseudos.length; i--;){
-				var pseudoClass = pseudos[parsedPseudos[i]];
-				if (!(pseudoClass && pseudoClass.call($u, node))) return false;
-			}
-		}
+        if ((parsedPseudos = parsed.pseudos)){
+            for (var i = parsedPseudos.length; i--;){
+                var pseudoClass = pseudos[parsedPseudos[i]];
+                if (!(pseudoClass && pseudoClass.call($u, node))) return false;
+            }
+        }
 
-		return true;
-	};
+        return true;
+    };
 
-	var find = function(){
+    var find = function(){
 
-		var parsedId = parsed.id,
-			merge = ((parsedId && parsed.tag || parsed.classes || parsed.pseudos)
-				|| (!parsedId && (parsed.classes || parsed.pseudos))) ?
-				arrayFilterAndMerge : arrayMerge;
+        var parsedId = parsed.id,
+            merge = ((parsedId && parsed.tag || parsed.classes || parsed.pseudos)
+                || (!parsedId && (parsed.classes || parsed.pseudos))) ?
+                arrayFilterAndMerge : arrayMerge;
 
-		if (parsedId){
+        if (parsedId){
 
-			var el = currentDocument.getElementById(parsedId);
-			if (el && (currentDocument === context || contains(el))){
-				merge([el]);
-			}
+            var el = currentDocument.getElementById(parsedId);
+            if (el && (currentDocument === context || contains(el))){
+                merge([el]);
+            }
 
-		} else {
+        } else {
 
-			merge(context.getElementsByTagName(parsed.tag || '*'));
+            merge(context.getElementsByTagName(parsed.tag || '*'));
 
-		}
+        }
 
-	};
+    };
 
-	var parse = function(selector){
-		parsed = {};
-		while ((selector = selector.replace(/([#.:])?([^#.:]*)/, parser))){};
-	};
+    var parse = function(selector){
+        parsed = {};
+        while ((selector = selector.replace(/([#.:])?([^#.:]*)/, parser))){};
+    };
 
-	var parser = function(all, simbol, name){
-		if (!simbol){
-			parsed.tag = name.toUpperCase();
-		} else if (simbol == '#'){
-			parsed.id = name;
-		} else if (simbol == '.'){
-			if (parsed.classes){
-				parsed.classes.push(name);
-			} else {
-				parsed.classes = [name];
-			}
-		} else if (simbol == ':'){
-			if (parsed.pseudos){
-				parsed.pseudos.push(name);
-			} else {
-				parsed.pseudos = [name];
-			}
-		}
-		return '';
-	};
+    var parser = function(all, simbol, name){
+        if (!simbol){
+            parsed.tag = name.toUpperCase();
+        } else if (simbol == '#'){
+            parsed.id = name;
+        } else if (simbol == '.'){
+            if (parsed.classes){
+                parsed.classes.push(name);
+            } else {
+                parsed.classes = [name];
+            }
+        } else if (simbol == ':'){
+            if (parsed.pseudos){
+                parsed.pseudos.push(name);
+            } else {
+                parsed.pseudos = [name];
+            }
+        }
+        return '';
+    };
 
-	var slice = Array.prototype.slice;
-	var arrayFrom = function(collection){
-		elements = slice.call(collection, 0);
-	};
-	var arrayMerge = function(collection){
-		for (var i = 0, node; node = collection[i++];){
-			elements.push(node);
-		}
-	};
-	try {
-		slice.call(document.documentElement.childNodes, 0);
-	} catch(e) {
-		arrayFrom = arrayMerge;
-	}
+    var slice = Array.prototype.slice;
+    var arrayFrom = function(collection){
+        elements = slice.call(collection, 0);
+    };
+    var arrayMerge = function(collection){
+        for (var i = 0, node; node = collection[i++];){
+            elements.push(node);
+        }
+    };
+    try {
+        slice.call(document.documentElement.childNodes, 0);
+    } catch(e) {
+        arrayFrom = arrayMerge;
+    }
 
-	var arrayFilterAndMerge = function(found){
-		for (var i = 0, node; node = found[i++];){
-			if (matchSelector(node)) elements.push(node);
-		}
-	};
+    var arrayFilterAndMerge = function(found){
+        for (var i = 0, node; node = found[i++];){
+            if (matchSelector(node)) elements.push(node);
+        }
+    };
 
-	var contains = function(node){
-		do {
-			if (node === context) return true;
-		} while ((node = node.parentNode));
-		return false;
-	};
+    var contains = function(node){
+        do {
+            if (node === context) return true;
+        } while ((node = node.parentNode));
+        return false;
+    };
 
-	$u['pseudos'] = pseudos;
-	$u['context'] = document;
-	global['uSelector'] = $u;
-	if (!global['$u']) global['$u'] = $u;
+    $u['pseudos'] = pseudos;
+    $u['context'] = document;
+    global['uSelector'] = $u;
+    if (!global['$u']) global['$u'] = $u;
 
 })(this, document);
--- tests/specs.js
+++ tests/specs.js
@@ -1,96 +1,96 @@
 var microSpecs = function(context){
 
 describe('Micro Selector', function() {
-	
-	var $u = uSelector;
-	$u.context = context.document;
-	
-	beforeEach(function(){
-		this.addMatchers({
-			toFind: function(n){
-				var found = $u(this.actual).length;
-				this.message = function() {
-					return [
-						"Expected " + this.actual + " to find " + n + " but found " + found,
-						"Expected " + this.actual + " not to find " + n
-					];
-				};
-				return found == n;
-			}
-		});
-	});
-	
-	it('should select elements by id', function(){
-		expect('#title').toFind(1);
-		expect('#other').toFind(0);
-		expect('#divid').toFind(1);
-		expect('#divid2').toFind(1);
-		expect($u('#divid', $u('#divid2')[0]).length).toEqual(0);
-	});
 
-	it('should select elements by tagName', function(){
-		var h2Collection = $u('h2');
-		expect(h2Collection instanceof Array).toEqual(true);
-		expect(h2Collection.length).toEqual(19);
-		expect('ul').toFind(22);
-		expect($u('div', $u('#divid')[0]).length).toEqual(4);
-	});
+    var $u = uSelector;
+    $u.context = context.document;
 
-	it('should select elements by className', function(){
-		var classVcardCollection = $u('.vcard');
-		expect(classVcardCollection instanceof Array).toEqual(true);
-		expect(classVcardCollection.length).toEqual(5);
-		expect('.pattern').toFind(40);
-		expect('.example').toFind(43);
-		expect($u('.inner', $u('.wrapper')[0]).length).toEqual(1);
-	});
-	
-	it('should select elements by tagName and id', function(){
-		expect('h1#title').toFind(1);
-		expect('div#title').toFind(0);
-	});
-	
-	it('should select elements by tagName and className', function(){
-		expect('div.wrapper').toFind(4);
-		expect('b.wrapper').toFind(1);
-	});
-	
-	it('should select elements with untrimed selectors', function(){
-		expect(' #title ').toFind(1);
-		expect(' ul ').toFind(22);
-		expect(' .example ').toFind(43);
-		expect(' h1#title ').toFind(1);
-		expect(' abbr.some#abbr-id.classes.here').toFind(1);
-	});
-	
-	it('should select elements with the added pseudo classes', function(){
-		$u.pseudos['contains-cheese'] = function(node){
-			return node.innerHTML.indexOf('cheese') > -1;
-		};
-		expect('div:contains-cheese').toFind(1);
-		expect('div:non-existent-pseudo').toFind(0);
-	});
-	
-	describe('Extra pseudo classes contained on the uSelectorPseudoClasses', function(){
-		
-		it('should find elements by the defined pseudo-classes', function(){
-			expect('a:empty').toFind(30);
-			
-			expect('p:first-child').toFind(54);
-			expect('p:last-child').toFind(19);
-			expect('p:only-child').toFind(3);
+    beforeEach(function(){
+        this.addMatchers({
+            toFind: function(n){
+                var found = $u(this.actual).length;
+                this.message = function() {
+                    return [
+                        "Expected " + this.actual + " to find " + n + " but found " + found,
+                        "Expected " + this.actual + " not to find " + n
+                    ];
+                };
+                return found == n;
+            }
+        });
+    });
 
-			expect('p:first-of-type').toFind(57);
-			expect('p:last-of-type').toFind(57);
-			expect('p:only-of-type').toFind(15);
-			
-			expect(':disabled').toFind(0);
-			expect(':checked').toFind(0);
-			expect(':selected').toFind(0);
-		});
-		
-	});
-	
+    it('should select elements by id', function(){
+        expect('#title').toFind(1);
+        expect('#other').toFind(0);
+        expect('#divid').toFind(1);
+        expect('#divid2').toFind(1);
+        expect($u('#divid', $u('#divid2')[0]).length).toEqual(0);
+    });
+
+    it('should select elements by tagName', function(){
+        var h2Collection = $u('h2');
+        expect(h2Collection instanceof Array).toEqual(true);
+        expect(h2Collection.length).toEqual(19);
+        expect('ul').toFind(22);
+        expect($u('div', $u('#divid')[0]).length).toEqual(4);
+    });
+
+    it('should select elements by className', function(){
+        var classVcardCollection = $u('.vcard');
+        expect(classVcardCollection instanceof Array).toEqual(true);
+        expect(classVcardCollection.length).toEqual(5);
+        expect('.pattern').toFind(40);
+        expect('.example').toFind(43);
+        expect($u('.inner', $u('.wrapper')[0]).length).toEqual(1);
+    });
+
+    it('should select elements by tagName and id', function(){
+        expect('h1#title').toFind(1);
+        expect('div#title').toFind(0);
+    });
+
+    it('should select elements by tagName and className', function(){
+        expect('div.wrapper').toFind(4);
+        expect('b.wrapper').toFind(1);
+    });
+
+    it('should select elements with untrimed selectors', function(){
+        expect(' #title ').toFind(1);
+        expect(' ul ').toFind(22);
+        expect(' .example ').toFind(43);
+        expect(' h1#title ').toFind(1);
+        expect(' abbr.some#abbr-id.classes.here').toFind(1);
+    });
+
+    it('should select elements with the added pseudo classes', function(){
+        $u.pseudos['contains-cheese'] = function(node){
+            return node.innerHTML.indexOf('cheese') > -1;
+        };
+        expect('div:contains-cheese').toFind(1);
+        expect('div:non-existent-pseudo').toFind(0);
+    });
+
+    describe('Extra pseudo classes contained on the uSelectorPseudoClasses', function(){
+
+        it('should find elements by the defined pseudo-classes', function(){
+            expect('a:empty').toFind(30);
+
+            expect('p:first-child').toFind(54);
+            expect('p:last-child').toFind(19);
+            expect('p:only-child').toFind(3);
+
+            expect('p:first-of-type').toFind(57);
+            expect('p:last-of-type').toFind(57);
+            expect('p:only-of-type').toFind(15);
+
+            expect(':disabled').toFind(0);
+            expect(':checked').toFind(0);
+            expect(':selected').toFind(0);
+        });
+
+    });
+
 });
 
 };
