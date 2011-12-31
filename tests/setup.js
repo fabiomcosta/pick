45d3f8f1c3a882ab63113cacf0fecc376820545e
Fabio Miranda Costa <fabiomcosta@gmail.com>
2011-12-31 00:11:13 -0300

    removing some documents since they dont make muck sense (they are supposed to act the same as the html5 document)

--- tests/setup.js
+++ tests/setup.js
@@ -8,10 +8,7 @@ var registerSpecs = function(){
 };
 
 var createMocks = function(){
-    Mock.CreateTemplate('Generic HTML4 (strict)',               'mocks/template-strict.php');
-    Mock.CreateTemplate('Generic HTML4 (transitional)',         'mocks/template-transitional.php');
     Mock.CreateTemplate('Generic HTML4 (quirks)',               'mocks/template-quirks.php');
-    Mock.CreateTemplate('Generic HTML4 (chromeframe)',          'mocks/template-chromeframe.php');
     Mock.CreateTemplate('Generic HTML5',                        'mocks/template-html5.php');
 
     if (Browser.ie && Browser.version >= 8){
